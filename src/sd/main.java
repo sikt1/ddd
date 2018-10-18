d2be9d2ac1401adef0a215a8304d083c3fa78599
admin <admin@121.186.13.27>
2018-10-19 00:44:26 +0900

    hi

---
+++ src/sd/main.java
@@ -0,0 +1,17 @@
+package sd;
+
+public class main {
+
+	public static void main(String[] args) {
+		int a ;
+		int b;
+		a=10;
+		b=20;
+		System.out.println("a=" +a);
+		System.out.println("b=" +b);
+			}
+
+
+	}
+
+
